652e5dfc500e33dd437c8d182635b3a358b986e4
Hasan  Yılmaz <33355796+hsnylmzz@users.noreply.github.com>
2019-06-16 20:29:07 +0300

    Create final 2.soru cevabi.c

---
+++ final 2.soru cevabi.c
@@ -0,0 +1,23 @@
+   #include <stdio.h> 
+   #include <stdlib.h> 
+   
+   int main()   
+   {     
+      int i,j; 
+      
+      for (i=0;i<12;i++){      // Hatali Satir No: 8.satir. Duzeltilmis Hali : for(i=0;i<11;i++)      
+            if (i<6) {                
+               for(j=0; j<=i; j++) printf("*");             
+               printf("\n");          
+              }else{            
+                 for(j=0; j< (10-i); j++) printf(" ");  //  Hatali Satir No: 13.satir. Duzeltilmis Hali : for(j=0;j<i-5;j++)         
+                 for(j=0; j< (i-5); j++) printf("*");   //  Hatali Satir No: 14.satir. Duzeltilmis Hali : for(j=0;j<11-i;j++)         
+                printf("\n");         
+             }    
+          }    
+          
+      return 0; 
+   }
+          
+    // Not : 1. Selcuk Hocanın Hata bulma sorusunda puan almaniz icin satir tamamen dogru duzeltilmis olmalidir.
+             2. Hatayi dogru bulup duzeltmezse veya yanlis duzeltilirse veya hatayi yanlis yerde bulursaniz puan alamazsiniz.
